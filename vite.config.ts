33e143102d02b305434cbd11dae64d829762d816
Hyegyeong Kim <ssfoxs@kakao.com>
2026-03-09 14:10:36 +0900

    fix: github action 맞춤 서브 경로

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/note-vis/' : '/',
   plugins: [viteReact(), tailwindcss()],
   test: {
     globals: true,
